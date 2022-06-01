f4980a18178726964a4feff959e95e668a9bad26
Steven Barsalou <steven.barsalou@gmail.com>
2022-06-01 18:52:59 -0500

    corrected alembic

--- app/models.py
+++ app/models.py
@@ -30,4 +30,3 @@ class Vote(Base):
     __tablename__ = "votes"
     post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
-    test_col = Column(Integer)
